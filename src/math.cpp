01fd1836bcaf6adb8fbde45801e18bee5357f846
Seymour Pashayev <gordonsamh@gmail.com>
2022-11-02 21:32:46 -0500

    math.cpp updated with vector stuff

--- src/math.cpp
+++ src/math.cpp
@@ -30,6 +30,21 @@ struct Vec2 {
         return sqrt(x*x + y*y);
     }
     
+    // Normalize
+    void normalize() {
+        float mag = this->getMagnitude();
+        
+        this->x /= mag;
+        this->y /= mag;
+    }
+    
+    // Perpendicular Vector
+    Vec2* perperdicular() {
+        Vec2* result = new Vec2(this->y, -this->x);
+        
+        return result;
+    }
+    
     // Vector Addition
     void add(Vec2 vec) {
         this->x += vec.x;
@@ -46,6 +61,11 @@ struct Vec2 {
     void scale(float val){
         this->x *= val;
         this->y *= val;
+    }
+    
+    // Dot Product
+    float dotProduct(Vec2 v){
+        return (this->x * v.x) + (this->y * v.y);
     }
     
     // ---- Static Vector Operations ----
@@ -68,6 +88,11 @@ struct Vec2 {
         return result;
     }
     
+    // Static Dot Product
+    static float dotProduct(Vec2 v1, Vec2 v2){
+        return (v1.x * v2.x) + (v1.y * v2.y);
+    }
+    
 }; //ENDOF: Vec2
 
 
@@ -86,12 +111,21 @@ struct Vec3 {
 
     // Destructor
     ~Vec3() = default;
-
+    
     // ---- Dynamic Vector Operations ----
     
     // Magnitude
     float getMagnitude(){
         return sqrt(x*x + y*y + z*z);
+    }
+    
+    // Normalize
+    void normalize() {
+        float mag = this->getMagnitude();
+        
+        this->x /= mag;
+        this->y /= mag;
+        this->z /= mag;
     }
     
     // Vector Addition
@@ -115,6 +149,22 @@ struct Vec3 {
         this->z *= val;
     }
     
+    // Dot (Scalar) Product - The output of the operation is NOT a vector
+    // (How aligned are the vectors from 1 being aligned to -1 being not aligned; 0 is a 90 angle)
+    float dotProduct(Vec3 v){
+        return (this->x * v.x) + (this->y * v.y) + (this->z * v.z);
+    }
+    
+    // Vector Cross Product
+    Vec3* crossProduct(Vec3 v){
+        Vec3* result = new Vec3(0,0,0);
+        result->x = (this->y * v.z) - (this->z * v.y);
+        result->y = (this->z * v.x) - (this->x * v.z);
+        result->z = (this->x * v.y) - (this->y * v.x);
+    
+        return result;
+    }
+    
     // ---- Static Vector Operations ----
     
     // Static Vector Addition
@@ -133,6 +183,21 @@ struct Vec3 {
         result->x = v1.x - v2.x;
         result->y = v1.y - v2.y;
         result->z = v1.z - v2.z;
+    
+        return result;
+    }
+    
+    // Static Dot Product
+    static float dotProduct(Vec3 v1, Vec3 v2){
+        return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+    }
+    
+    // Static Cross Product
+    static Vec3* crossProduct(Vec3 v1, Vec3 v2){
+        Vec3* result = new Vec3(0,0,0);
+        result->x = (v1.y * v2.z) - (v1.z * v2.y);
+        result->y = (v1.z * v2.x) - (v1.x * v2.z);
+        result->z = (v1.x * v2.y) - (v1.y * v2.x);
     
         return result;
     }
